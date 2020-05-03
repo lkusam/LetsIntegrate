1b9db8af6b2f3d59626be2f30a39b7d796719f40
lkusam <lkusam@kent.edu>
2020-05-03 15:02:00 -0400

    Lastest check-in

--- JS/index.js
+++ JS/index.js
@@ -2,8 +2,4 @@
     // var elems = document.querySelectorAll('.parallax');
     // var instances = M.Parallax.init(elems);
     M.AutoInit();
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'right'
-    });
   });
--- JS/stringops.js
+++ JS/stringops.js
@@ -1,13 +1,29 @@
-  document.addEventListener('DOMContentLoaded', function () {
-      M.AutoInit();
-  });
+'use strict'
+document.addEventListener('DOMContentLoaded', function () {
+    M.AutoInit();
+});
 
-  document.getElementById('userinput').addEventListener("keyup", function () {
-      var inputtext = document.getElementById('userinput').value;
-      if (inputtext) {
-          document.getElementById('lengthres').innerHTML = inputtext.length;
-          document.getElementById('encoderes').innerHTML = encodeURIComponent(inputtext);
-          document.getElementById('decoderes').innerHTML = decodeURIComponent(inputtext);
-      } else
-          document.getElementById('lengthres').innerHTML = 'Please enter the text';
-  })
+document.getElementById('userinput').addEventListener("keyup", function () {
+    let inputtext;
+    inputtext = document.getElementById('userinput').value;
+    if (inputtext) {
+        document.getElementById('lengthres').innerHTML = inputtext.length;
+        document.getElementById('encoderes').innerHTML = encodeURIComponent(inputtext);
+        document.getElementById('decoderes').innerHTML = decodeURIComponent(inputtext);
+        setRegex(inputtext);
+    } else
+        document.getElementById('lengthres').innerHTML = 'Please enter the text';
+});
+/**
+ * Gets the given regex matching string
+ * @param {string} inputval Given input string from the user
+ */
+function setRegex(inputval) {
+    let givenRegex = document.getElementById('regex_given').value;
+    if (givenRegex) {
+        //let regex = new RegExp(givenRegex);
+        let executeStr = inputval.match(givenRegex);
+        document.getElementById('regex_result').innerHTML = `${executeStr}`;
+    }
+
+}
